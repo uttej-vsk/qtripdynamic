6cb1f374d3e2d55f2f5a26dd316de19208d71bcb
Uttej Venkata Sastry <uttejvsk@hotmail.com>
2022-04-23 17:34:01 +0530

    Adding the elements using function in automating

--- index.js
+++ index.js
@@ -1,9 +1,20 @@
-/*console.log("Hello, from JS!");*/
-// const element = document.getElementsByTagName("h1");
-// const ele = element[0];
-// console.log(ele.innerHTML="Practicing DOM Manipulation!");
-const para = document.createElement("p");
-para.innerHTML= "innerText property refers to the text inside an element";
+// /*console.log("Hello, from JS!");*/
+// // const element = document.getElementsByTagName("h1");
+// // const ele = element[0];
+// // console.log(ele.innerHTML="Practicing DOM Manipulation!");
+// const para = document.createElement("p");
+// para.innerHTML= "innerText property refers to the text inside an element";
 
-const divEle = document.getElementById("facts-list");
-divEle.appendChild(para);
+// const divEle = document.getElementById("facts-list");
+// divEle.appendChild(para);
+
+function addNewFact(factText) {
+	const para = document.createElement("p");
+	para.innerHTML = factText;
+
+	const divEle = document.getElementById("facts-list");
+	divEle.appendChild(para);
+}
+
+addNewFact("innerText property refers to the text inside an element");
+addNewFact("append() or appendChild() methods can add a new child element");
