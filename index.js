d928c8d1b19fa4c77a9646a0a53dd4554d84ee2d
Uttej Venkata Sastry <uttejvsk@hotmail.com>
2022-04-23 17:03:06 +0530

    first dom manipulation using getElementByTagname

--- index.js
+++ index.js
@@ -1,1 +1,4 @@
-console.log("Hello, from JS!");
+/*console.log("Hello, from JS!");*/
+const element = document.getElementsByTagName("h1");
+const ele = element[0];
+console.log(ele.innerHTML="Practicing DOM Manipulation!");
