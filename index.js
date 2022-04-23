c900e3f1f24d6c616c35ffa1c55ed0939d171cb2
Uttej Venkata Sastry <uttejvsk@hotmail.com>
2022-04-23 17:46:19 +0530

    replacing the content inside the HTML elements using innerHTML

--- index.js
+++ index.js
@@ -1,20 +1,25 @@
-// /*console.log("Hello, from JS!");*/
-// // const element = document.getElementsByTagName("h1");
-// // const ele = element[0];
-// // console.log(ele.innerHTML="Practicing DOM Manipulation!");
-// const para = document.createElement("p");
-// para.innerHTML= "innerText property refers to the text inside an element";
+// // /*console.log("Hello, from JS!");*/
+// // // const element = document.getElementsByTagName("h1");
+// // // const ele = element[0];
+// // // console.log(ele.innerHTML="Practicing DOM Manipulation!");
+// // const para = document.createElement("p");
+// // para.innerHTML= "innerText property refers to the text inside an element";
 
-// const divEle = document.getElementById("facts-list");
-// divEle.appendChild(para);
+// // const divEle = document.getElementById("facts-list");
+// // divEle.appendChild(para);
 
-function addNewFact(factText) {
-	const para = document.createElement("p");
-	para.innerHTML = factText;
+// function addNewFact(factText) {
+// 	const para = document.createElement("p");
+// 	para.innerHTML = factText;
 
-	const divEle = document.getElementById("facts-list");
-	divEle.appendChild(para);
-}
+// 	const divEle = document.getElementById("facts-list");
+// 	divEle.appendChild(para);
+// }
 
-addNewFact("innerText property refers to the text inside an element");
-addNewFact("append() or appendChild() methods can add a new child element");
+// addNewFact("innerText property refers to the text inside an element");
+// addNewFact("append() or appendChild() methods can add a new child element");
+
+document.getElementById("facts-list").innerHTML = 
+`<p><i>Browser</i> creates DOM from HTML content</p>
+<p>Developers can access the DOM using the DOM APIs</p>
+<p>innerHTML property refers to the HTML inside an element</p>`;
