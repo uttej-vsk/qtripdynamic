2fcf84184ccdf405019185dfa3766bfaade6962a
Uttej Venkata Sastry <uttejvsk@hotmail.com>
2022-04-23 17:14:31 +0530

    Adding a new element in the body using append and getElementById

--- index.js
+++ index.js
@@ -1,4 +1,9 @@
 /*console.log("Hello, from JS!");*/
-const element = document.getElementsByTagName("h1");
-const ele = element[0];
-console.log(ele.innerHTML="Practicing DOM Manipulation!");
+// const element = document.getElementsByTagName("h1");
+// const ele = element[0];
+// console.log(ele.innerHTML="Practicing DOM Manipulation!");
+const para = document.createElement("p");
+para.innerHTML= "innerText property refers to the text inside an element";
+
+const divEle = document.getElementById("facts-list");
+divEle.appendChild(para);
